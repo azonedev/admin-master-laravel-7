7ac166719faa4e13e0e2523bcca383e59ba1b716
Abdullah Al Mamun <abdullah914115@gmail.com>
2020-10-24 17:18:33 +0600

    admin-master-laravel-7

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+// --------------
+// Frontend
+// --------------
+
+
+Route::get('/', 'frontend\Home@index');
+
+// --------------
+// Admin
+// --------------
+
+// Route::group(['prefix'=>'admin', 'middleware' =>'role'],function (){
+
+//     Route::get('/',"admin\Admin@index");
+
+// });
+
+// --------------
+// User
+// --------------
+
+// user login & reg
+
+// Route::get('/register',"user\GeneralUser@index");
+// Route::post('/register/save',"user\GeneralUser@store");
+
+// Route::get('/login/{url?}',"user\GeneralUser@showLogin");
+// Route::post('/login/match',"user\GeneralUser@matchLogin");
+// Route::get('/logout',"user\GeneralUser@logout");
+                                                                                        
+// all-user-action
+
+// Route::group(['prefix' => 'user','middleware' =>'useraction'], function () {
+
+// });
+
